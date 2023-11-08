3c3de9e8e78cf21a47cfe66c39cad9a3808488bb
DoDuong1497 <duongdo1497@gmail.com>
2023-11-08 23:37:57 +0700

    update

--- js/main.js
+++ js/main.js
@@ -11,8 +11,26 @@ lightGallery(document.getElementById('gallery-image'), {
   },
 });
 
-setTimeout(() => {
-  const audio = document.querySelector('audio');
-  audio.volume = 0.5;
-  audio.play();
-}, '100');
+let wedding = document.querySelector('#music-wedding');
+let audio = document.querySelector('audio');
+
+wedding.addEventListener('click', (e) => {
+  let _this = e.currentTarget;
+  let iconMusic = wedding.querySelector('img');
+
+  _this.classList.toggle('active');
+
+  if (_this.className === 'active') {
+    audio.volume = 0.5;
+    audio.play();
+    audio.loop = true;
+
+    iconMusic.src = './image/volume-mute.png';
+    iconMusic.setAttribute('title', 'Tắt nhạc');
+  } else {
+    audio.pause();
+
+    iconMusic.src = './image/music-player.png';
+    iconMusic.setAttribute('title', 'Bật nhạc');
+  }
+});
